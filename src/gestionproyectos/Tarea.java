4e74c222f39d698fbc2b4d9e5a39ef9bb9db7818
hpsopastech <andres.ar.tech@gmail.com>
2026-08-17 13:53:30 -0500

    Agregar constructor de tarea

--- src/gestionproyectos/Tarea.java
+++ src/gestionproyectos/Tarea.java
@@ -1,3 +1,9 @@
 public class Tarea {
+    private String nombre;
+    private boolean completada;
 
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+        this.completada = false;
+    }
 }
